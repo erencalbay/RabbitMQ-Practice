e96f876303382ae7a74f54d9bd9952246a18279d
Eren Çalbay <calbayeren@gmail.com>
2025-02-02 23:24:11 +0300

    Simply Consumer-Producer

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Kuyruk Oluşturma                      //Kuyruk yapılandırması Publishler ile aynı olmalıdır.
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Kuyrukdan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir
+    // e.Body = Kuyruktaki mesajın veriyi bütünsel olarak getirecektir.
+    // e.Body.Span veya e.Body.ToArray() = Kuyruktaki mesajın byte verisini getirecektir
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//CloudAMQP üzerinden alınan URI ile bağlantı sağlama
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantıyı aktifleştirip kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Kuyruk oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Kuyruğa mesaj gönderme         //RabbitMQ kuyruğa atacağı mesajları byte türünde kabul ediyor, o yüzden mesajları byte'a dönüştürülmeli
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message); //exchange boş bırakırsak **direct exchange** kullanılır
+
+Console.Read();
