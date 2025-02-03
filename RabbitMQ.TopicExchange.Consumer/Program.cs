0bbaa1c3fc4dab769e54e0539073d23031b0fafc
Eren Çalbay <calbayeren@gmail.com>
2025-02-03 17:01:51 +0300

    Topic Exchange

---
+++ RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Publisherda tanımlanan Topic Exchange'i birebir aynı şekilde tanımlıyoruz
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+//Sub olunacak topici istiyoruz ve kuyruğu declare ediyoruz
+Console.WriteLine("Dinlenicek Topic Formatını Belirtiniz: ");
+string topic = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName; // Belirtilmek istenmediğinde bu şekilde RabbitMQ random olarak queue ismi atacaktır
+
+//Exchange ile Kuyruğu bind ediyoruz
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example", 
+    routingKey: topic);
+
+//Tüketilecek
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+//Mesajı alıyoruz
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Topic Exchange'i tanımlıyoruz
+channel.ExchangeDeclare(
+    exchange:"topic-exchange-example",
+    type:ExchangeType.Topic
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    Console.WriteLine("Mesajın Gönderileceği Topic Formatını Belirtiniz : ");
+    string topic = Console.ReadLine();
+
+    //Mesajın hangi topic altında olacağını kullanıcıdan aldıktan sonra routingKey ile publish ediyoruz
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message
+        );
+}
+
+Console.Read();
