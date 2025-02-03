9c968a63e1d42bca4da8c1a4e9dcefd9c225e9b9
Eren Çalbay <calbayeren@gmail.com>
2025-02-03 17:26:11 +0300

    Header Exchange

---
+++ RabbitMQ.HeaderExchange.Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Publisherda tanımlanan Header Exchange'i birebir aynı şekilde tanımlıyoruz
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+//Sub olunacak headerı istiyoruz ve kuyruğu declare ediyoruz
+Console.WriteLine("Lütfen Header Değerini Giriniz: ");
+string value = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName; // Belirtilmek istenmediğinde bu şekilde RabbitMQ random olarak queue ismi atacaktır
+
+//Exchange ile Kuyruğu bind ediyoruz
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange-example",
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        ["no"] = value
+    }
+    );
+
+//Tüketilecek
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+//Mesajı alıyoruz
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.HeaderExchange.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Header Exchange'i tanımlıyoruz
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    Console.WriteLine("Lütfen Header Değerini Giriniz : ");
+    string value = Console.ReadLine();
+
+    //Headerı burada tanımlıyoruz ve kullanıcıdan aldığımız value'yu basicProperties nesnesine atıyoruz
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string,object>()
+    {
+        ["no"] = value
+    };
+
+    //Publish ederken basicPropertiese karşılık olarak headerı veriyoruz
+    channel.BasicPublish(
+        exchange: "header-exchange-example",
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties
+        );
+}
+
+Console.Read();
