9b4945a748f8d95cce625400d1182442a392ddac
Eren Çalbay <calbayeren@gmail.com>
2025-02-03 15:38:23 +0300

    Direct Exchange

---
+++ RabbitMQ.DirectExchange.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿//Bağlantı Oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Publisherda tanımlanan exchange aynı şekilde tanımlanmalı
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+//Kuyruğu tanımlıyoruz
+string queueName = channel.QueueDeclare().QueueName;
+
+//Binding işlemi-routingKey ile kuyruğa sub oluyoruz ve dinliyoruz
+channel.QueueBind(
+    queue: queueName,
+    exchange: "direct-exchange-example",
+    routingKey: "direct-queue-example");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿//Bağlantı Oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bpgwdknj:V_pdZZSLy3C60dgHPB87KSRAwqgaKgiz@gull.rmq.cloudamqp.com/bpgwdknj");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Exchange'i direct olarak tanımlıyoruz ve isimlendiriyoruz
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.WriteLine("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] bytemessage = Encoding.UTF8.GetBytes(message);
+
+    //Gönderilecek olan mesajın hangi kuyruğa gideceğini routing key ile belirtiyoruz
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example",
+        body: bytemessage);
+}
+
+Console.Read();
